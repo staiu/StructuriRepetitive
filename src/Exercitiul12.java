c5e5783797aaf07a492d2c21ac32317706375567
HP-PC <maristaiu@yahoo.com>
2019-05-05 07:46:56 -0700

    Exercitiu12

---
+++ src/Exercitiul12.java
@@ -0,0 +1,32 @@
+public class Exercitiul12 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        switch(n) {
+            case 11:
+            case 0:
+            case 1:
+                System.out.println("este iarna");
+                break;
+            case 2:
+            case 3:
+            case 4:
+                System.out.println("este primavara");
+                break;
+            case 5:
+            case 6:
+            case 7:
+                System.out.println("este vara");
+                break;
+            case 8:
+            case 9:
+            case 10:
+                System.out.println("este toamna");
+                break;
+            default:
+
+
+
+        }
+    }
+}
