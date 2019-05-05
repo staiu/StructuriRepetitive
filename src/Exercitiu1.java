f333a4f7dd0fad41fb6914e4979fe7ed0302c30e
HP-PC <maristaiu@yahoo.com>
2019-05-05 05:06:09 -0700

    Exercitiu5

--- src/Exercitiu1.java
+++ src/Exercitiu1.java
@@ -1,12 +1,5 @@
 public class Exercitiu1 {
     public static void main(String[] args) {
-        int n = 23;
-        String diezuri = "";
 
-        for (int i = 0; i < n; i++) {
-            diezuri += "#";
-        }
-
-        System.out.println(diezuri);
     }
 }
---
+++ src/Exercitiu5.java
@@ -0,0 +1,10 @@
+public class Exercitiu5 {
+    public static void main(String[] args) {
+
+        for (int i = 6; i > 0; i--) {
+            System.out.println(i);
+
+        }
+    }
+
+}
